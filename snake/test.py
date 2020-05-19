d2c2ede64a139ea5dfd42d1cd6f3fb4975a16ff5
Manan Soni <manansoni1399@gmail.comclear>
2020-05-19 12:15:44 +0530

    Add weights for 3x3 grid

--- snake/test.py
+++ snake/test.py
@@ -4,7 +4,7 @@ import numpy as np
 np.random.seed()
 
 N = 3
-mem = 0
+mem = 3
 #snake = HumanSnake()
 snake = AISnake(N=N, mem=mem, alpha=0.9, weights=f'final_weights/{N}x{N}-{mem}.json')
 game = Game(snake,N)
